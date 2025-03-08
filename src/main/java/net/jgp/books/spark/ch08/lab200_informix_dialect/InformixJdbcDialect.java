86126ff763aee18b2fc42c66ec123599bce8a542
Maks00071 <corystov.maks@yandex.ru>
2025-03-08 18:39:26 +0300

    ADD ch08/lab200_informix_dialect InformixJdbcDialect.java and InformixToDatasetApp.java

--- src/main/java/net/jgp/books/spark/ch08/lab200_informix_dialect/InformixJdbcDialect.java
+++ src/main/java/net/jgp/books/spark/ch08/lab200_informix_dialect/InformixJdbcDialect.java
@@ -38,34 +38,34 @@ public class InformixJdbcDialect extends JdbcDialect {
         return url.startsWith("jdbc:informix-sqli");        // {6}
     }
 
-    @Override
-    public Option<DataType> getCatalystType(int sqlType, String typeName, int size, MetadataBuilder md) {   // {7}
-        if (typeName.toLowerCase().compareTo("serial") == 0) {  // {8}
-            return Option.apply(DataTypes.IntegerType);      // {9}
-        }
-        if (typeName.toLowerCase().compareTo("calendar") == 0) {
-            return Option.apply(DataTypes.BinaryType);      // {9}
-        }
-        if (typeName.toLowerCase().compareTo("calendarpattern") == 0) {
-            return Option.apply(DataTypes.BinaryType);      // {9}
-        }
-        if (typeName.toLowerCase().compareTo("se_metadata") == 0) {
-            return Option.apply(DataTypes.BinaryType);      // {9}
-        }
-        if (typeName.toLowerCase().compareTo("sysbldsqltext") == 0) {
-            return Option.apply(DataTypes.BinaryType);      // {9}
-        }
-        if (typeName.toLowerCase().compareTo("timeseries") == 0) {
-            return Option.apply(DataTypes.BinaryType);      // {9}
-        }
-        if (typeName.toLowerCase().compareTo("st_point") == 0) {
-            return Option.apply(DataTypes.BinaryType);      // {9}
-        }
-        if (typeName.toLowerCase().compareTo("tspartitiondesc_t") == 0) {
-            return Option.apply(DataTypes.BinaryType);      // {9}
-        }
-        return Option.empty();
-    }
+//    @Override
+//    public Option<DataType> getCatalystType(int sqlType, String typeName, int size, MetadataBuilder md) {   // {7}
+//        if (typeName.toLowerCase().compareTo("serial") == 0) {  // {8}
+//            return Option.apply(DataTypes.IntegerType);      // {9}
+//        }
+//        if (typeName.toLowerCase().compareTo("calendar") == 0) {
+//            return Option.apply(DataTypes.BinaryType);      // {9}
+//        }
+//        if (typeName.toLowerCase().compareTo("calendarpattern") == 0) {
+//            return Option.apply(DataTypes.BinaryType);      // {9}
+//        }
+//        if (typeName.toLowerCase().compareTo("se_metadata") == 0) {
+//            return Option.apply(DataTypes.BinaryType);      // {9}
+//        }
+//        if (typeName.toLowerCase().compareTo("sysbldsqltext") == 0) {
+//            return Option.apply(DataTypes.BinaryType);      // {9}
+//        }
+//        if (typeName.toLowerCase().compareTo("timeseries") == 0) {
+//            return Option.apply(DataTypes.BinaryType);      // {9}
+//        }
+//        if (typeName.toLowerCase().compareTo("st_point") == 0) {
+//            return Option.apply(DataTypes.BinaryType);      // {9}
+//        }
+//        if (typeName.toLowerCase().compareTo("tspartitiondesc_t") == 0) {
+//            return Option.apply(DataTypes.BinaryType);      // {9}
+//        }
+//        return Option.empty();
+//    }
 
 }
 
